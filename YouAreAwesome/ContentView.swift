662aa76ecbf50594708eae58860fca45ddfbe118
Timothy Hays <timothy.hays@bc.edu>
2025-01-18 18:54:28 -0500

    Update

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,22 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var message = "I am a programmer!"
+    @State private var message = "I Am a Programmer!"
     var body: some View {
         VStack {
+            Spacer()
+            
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200
+                )
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            Button("Click Me!") {
-                message = "Awesome"
-                print(message)
-                
-            }
+                .fontWeight(.thin)
+                .foregroundStyle(.black)
+           
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    print(message)
+                }
+                Spacer()
+                Button("Great") {
+                    print(message)
+                }
+            }.buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.orange)
         }
         .padding()
     }
